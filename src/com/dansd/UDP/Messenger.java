462ddcb573f7a283eb19bc670ada5f784c767223
Francisco Dans <fdansv@gmail.com>
2013-11-06 17:13:13 +0000

    Messenger is now a nice class for public use

---
+++ src/com/dansd/UDP/Messenger.java
@@ -0,0 +1,65 @@
+package com.dansd.UDP;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: franciscodans
+ * Date: 06/11/2013
+ * Time: 13:03
+ * To change this template use File | Settings | File Templates.
+ */
+public class Messenger {
+    private int sessionPort;
+    private String sessionHost;
+
+    public Messenger(){
+        this("localhost");
+    }
+    public Messenger(String host){
+        this(host, 9876);
+    }
+    public Messenger(String host, int port){
+        sessionPort = port;
+        sessionHost = host;
+    }
+    public void send(String message){
+        DatagramSocket clientSocket = null;
+        try {
+            clientSocket = new DatagramSocket();
+        } catch (SocketException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        InetAddress IP = null;
+        try {
+            IP = InetAddress.getByName(sessionHost);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        byte[] receiveData = new byte[1024];
+        byte[] sendData = message.getBytes();
+        DatagramPacket sendPacket  = new DatagramPacket(sendData, sendData.length, IP, sessionPort);
+        try {
+            clientSocket.send(sendPacket);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        try {
+            clientSocket.receive(receivePacket);
+        } catch (IOException e) {
+            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+        }
+        String responseString = new String(receivePacket.getData());
+        onResponse(responseString);
+        clientSocket.close();
+        }
+
+    public void onResponse(String response){
+        System.out.println("RESPONSE: "+response);
+    }
+
+}
--- src/com/dansd/UDP/UDPTest.java
+++ src/com/dansd/UDP/UDPTest.java
@@ -7,6 +7,7 @@ import java.net.InetAddress;
 public class UDPTest {
     static int port = 9876;
     public static void main(String[] args) throws Exception{
+        new Messenger("localhost").send("hello, I'm fran");
     }
 
 
