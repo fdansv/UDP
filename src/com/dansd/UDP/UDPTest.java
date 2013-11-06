6daedf94806755732029d332e98cab28a578d42c
Francisco Dans <francisco.dans@images.co.uk>
2013-11-06 12:58:44 +0000

    adding client

--- src/com/dansd/UDP/UDPTest.java
+++ src/com/dansd/UDP/UDPTest.java
@@ -7,6 +7,25 @@ import java.net.InetAddress;
 public class UDPTest {
     static int port = 9876;
     public static void main(String[] args) throws Exception{
+        send("Hey dude!");
+    }
+    private static void send(String stringToSend) throws Exception{
+        System.out.println("Sending");
+        DatagramSocket clientSocket = new DatagramSocket();
+        InetAddress IP = InetAddress.getByName("192.168.11.13");
+        byte[] sendData = new byte[1024];
+        byte[] receiveData = new byte[1024];
+        sendData = stringToSend.getBytes();
+        DatagramPacket sendPacket  = new DatagramPacket(sendData, sendData.length, IP, port);
+        clientSocket.send(sendPacket);
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+        clientSocket.receive(receivePacket);
+        String responseString = new String(receivePacket.getData());
+        System.out.println("RESPONSE: "+responseString);
+        clientSocket.close();
+
+    }
+    private static void listen() throws Exception{
         DatagramSocket serverSocket = new DatagramSocket(port);
         byte[] receiveData = new byte[1024];
         byte[] sendData = new byte[24];
